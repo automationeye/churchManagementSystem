6eb4197232176e1ff0e67d436068c8109706c5b0
donwuzu <davmurage@gmail.com>
2024-02-07 19:36:16 +0300

    mobile interaction

--- resources/views/admin/admin/layouts/main.blade.php
+++ resources/views/admin/admin/layouts/main.blade.php
@@ -9,6 +9,7 @@
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 
     <link href="admin/resorce/css/style.css" rel="stylesheet">
@@ -16,6 +17,10 @@
     <style>
         .hidden {
             display: none;
+        }
+
+        @media (max-width: 767px) {
+            /* CSS rules for screens smaller than 768px */
         }
     </style>
 
